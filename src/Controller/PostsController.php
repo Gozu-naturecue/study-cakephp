967b132526d395eacbb256838060d96484c6f585
Yuhsuke Gozu <Yuhsuke_Gozu@nature-cue.com>
2018-03-03 14:41:30 +0900

    記事コントローラの追加

---
+++ src/Controller/PostsController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Posts Controller
+ *
+ * @property \App\Model\Table\PostsTable $Posts
+ *
+ * @method \App\Model\Entity\Post[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class PostsController extends AppController
+{
+
+    public function index()
+    {
+    }
+}
